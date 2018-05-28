829bc336a35baf2b2073c16177f706e161ca701f
madelineyao <yao.madeline@gmail.com>
2018-05-28 15:40:29 -0800

    created the buttons and show the first 10 people on webpage

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
 //Plans to write the code: 
-// a. Calculate the number of pages 
-// 
-// b. Show the search bar 
-// c. Load the page into 10 individuals per page 
+//
+// a. Load the page into 10 individuals per page(priority)
+//    create the button --> let the original page disappear ---> show the first 10 people 
+//     ----> assign events to buttons 
+// b. add people to the webpage by JavaScript 
+//     locate the object(the place to add and the object added) --> order the one object to add another
 
-const people = document.getElementsByTagName('li'); 
+const people = document.getElementsByClassName("student-item"); 
+const pages = getNumberOfPages(people); 
+const webpage = document.getElementsByClassName('page'); 
+
+
+const buttons = document.createElment('div');
+buttons.className = 'pagination'; 
+const button_list = document.createElement('ul'); 
+buttons.appendChild(button_list); 
+for(let i=0; i<pages; i+=1){
+    let button = document.createElement('li'); 
+    let atag = document.createElement('a'); 
+    button.appendChild(atag); 
+    atag.textContent = i; 
+    button_list.appendChild(button); 
+    
+}
+
+webpage[0].appendChild(buttons); 
+
+hideAll(); 
+
+for(let i = 0; i< 10; i+=1){
+    people[i].style.display = 'block'; 
+}
+
+
+
+
+
+
 
 function getNumberOfPages(lists){
     let num = lists.length; 
@@ -15,4 +47,12 @@ function getNumberOfPages(lists){
     }while(num>0)
     return page; 
 }
+
+function hideAll(){
+    for(let i = 0; i < people.length; i+=1){
+        people[i].style.display = 'none'; 
+    }
+}
+
+ 
 
